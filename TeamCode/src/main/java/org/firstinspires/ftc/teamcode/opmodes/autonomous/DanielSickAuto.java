d367fd3715255f22e1ac12d5ce54584ae34d23fb
danieldb <danielduartebaird@gmail.com>
2024-11-26 21:19:06 -0500

    fix auto package in DanielSickAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/DanielSickAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/DanielSickAuto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes.teleop;
+package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
 import static org.firstinspires.ftc.teamcode.robot.Hobbes.helpers.HobbesConstants.*;
 import static org.firstinspires.ftc.teamcode.robot.Hobbes.helpers.Macros.*;
