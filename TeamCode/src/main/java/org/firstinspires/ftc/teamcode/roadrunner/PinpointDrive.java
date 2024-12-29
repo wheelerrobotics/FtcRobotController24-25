d86c2ef7bf6e7f141835fd31ac7a4ab9dc9e3b67
davidyyyyyyyyy <dbattlecardemil@gmail.com>
2024-12-29 14:05:55 -0500

    pinpoint setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/PinpointDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/PinpointDrive.java
@@ -40,8 +40,12 @@ public class PinpointDrive extends MecanumDrive {
          */
         //These are tuned for 3110-0002-0001 Product Insight #1
         // RR localizer note: These units are inches, presets are converted from mm (which is why they are inexact)
-        public double xOffset = 3.307;
-        public double yOffset = -2.25;
+     //   public double xOffset = 3.307;
+       // public double yOffset = -2.25;
+
+       public double xOffset = 3.30709;
+        public double yOffset = 4.40945;
+
 
         /*
         Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
