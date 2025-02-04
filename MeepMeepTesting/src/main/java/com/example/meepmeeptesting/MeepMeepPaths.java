5191ae8ecfae771f2efd3300611042b43ee9f4fd
davidyyyyyyyyy <dbattlecardemil@gmail.com>
2025-02-04 12:38:30 -0500

    double submersible?

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepPaths.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepPaths.java
@@ -24,7 +24,10 @@ public class MeepMeepPaths {
                 // Declare a MeepMeep instance
                 // With a field size of 800 pixels
                 MeepMeep meepMeep = new MeepMeep(600);
-
+                        double x= 0;
+                double y= 10;
+                double x2= 0;
+                double y2= 10;
                 RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                         .setDimensions(12, 15)
                         .setStartPose(new Pose2d(-63, 6, PI))
@@ -39,57 +42,48 @@ public class MeepMeepPaths {
                 //Splines for first spike mark and first bucket
                 TrajectoryActionBuilder s1 = b0.endTrajectory().fresh()
                         .setTangent(PI/2)
-                        .splineToConstantHeading(new Vector2d(-10, 10), PI/2)
-                        .splineToSplineHeading(new Pose2d(-10, 20, PI/2), PI/2)
-                        .splineToConstantHeading(new Vector2d(-10, 23), PI/2)
-                        ;
+                        .splineToConstantHeading(new Vector2d(-11, 10), PI/2)
+                        .splineToLinearHeading(new Pose2d(-11, 18, PI/2), PI/2, null, new ProfileAccelConstraint(-20, 20));
 
                 TrajectoryActionBuilder b1 = s1.endTrajectory().fresh()
                         .setTangent(-PI/2)
-                        .splineToLinearHeading(new Pose2d(-13.5, 10.7, PI/4), -3*PI/4)
-                        ;
+                        .splineToLinearHeading(new Pose2d(-15, 8, PI/4), -3*PI/4);
                 //second spike mark
                 TrajectoryActionBuilder s2 = b1.endTrajectory().fresh()
                         .setTangent(PI/2)
-                        .splineToLinearHeading(new Pose2d(-21, 20, PI/2), PI/2)
+                        .splineToLinearHeading(new Pose2d(-20, 18, PI/2), PI/2, null, new ProfileAccelConstraint(-20, 20))
                         ;
 
                 TrajectoryActionBuilder b2 = s2.endTrajectory().fresh()
                         .setTangent(-PI/2)
-                        .splineToLinearHeading(new Pose2d(-24, 15, PI/2), -PI/2);
+                        .splineToLinearHeading(new Pose2d(-28, 12, PI/2), -PI/2);
 
                 TrajectoryActionBuilder s3 = b2.endTrajectory().fresh()
                         .setTangent(0)
-                        .splineToLinearHeading(new Pose2d(-20, 20, Math.toRadians(130)), Math.toRadians(130));
+                        .splineToLinearHeading(new Pose2d(-12, 27, Math.toRadians(145)), Math.toRadians(145), null, new ProfileAccelConstraint(-20, 20));
 
                 TrajectoryActionBuilder b3 = s3.endTrajectory().fresh()
                         .setTangent(-PI/2)
-                        .splineToLinearHeading(new Pose2d(-15, 15, PI/3), -4*PI/6);
-
+                        .splineToLinearHeading(new Pose2d(-15, 8, PI/4), -3*PI/4);
 
 
                 TrajectoryActionBuilder s4 = b3.endTrajectory().fresh()
                         .setTangent(PI/4)
-                        .splineToLinearHeading(new Pose2d(5, 50, 0), 0);
-
+                        .splineToLinearHeading(new Pose2d(6+x, 43+y, 0), 0, null, new ProfileAccelConstraint(-50, 50))
+                      ;
                 TrajectoryActionBuilder b4 = s4.endTrajectory().fresh()
                         .setTangent(PI)
-                        .splineTo(new Vector2d(-20.5, 10.7), -3*PI/4);
+                        .splineToLinearHeading(new Pose2d(-13, 15, PI/4), -3*PI/4, null, new ProfileAccelConstraint(-40, 80));
 
                 TrajectoryActionBuilder s5 = b4.endTrajectory().fresh()
                         .setTangent(PI/4)
-                        .splineTo(new Vector2d(5, 50), 0);
+                        .splineToLinearHeading(new Pose2d(6+x2, 43+y2, 0), 0, null, new ProfileAccelConstraint(-50, 50));
 
                 TrajectoryActionBuilder b5 = s5.endTrajectory().fresh()
                         .setTangent(PI)
-                        .splineTo(new Vector2d(-20.5, 10.7), -3*PI/4);
+                        .splineToLinearHeading(new Pose2d(-13, 15, PI/4), -3*PI/4, null, new ProfileAccelConstraint(-40, 80));
 
 
-
-
-                TrajectoryActionBuilder p1  =b5.endTrajectory().fresh()
-                        .setTangent(-5*PI/4)
-                        .splineTo(new Vector2d(-15, 50), 0);
 
                 //build all of the splines
                 Action bucket0 = b0.build();
@@ -99,13 +93,12 @@ public class MeepMeepPaths {
                 Action bucket2 = b2.build();
                 Action pickup3 = s3.build();
                 Action bucket3 = b3.build();
-
                 Action submer4 = s4.build();
                 Action bucket4 = b4.build();
                 Action submer5 = s5.build();
                 Action bucket5 = b5.build();
-                Action park = p1.build();
-                myBot.runAction(new SequentialAction(bucket0, pickup1, bucket1, pickup2, bucket2, pickup3, bucket3, submer4, bucket4, submer5, bucket5
+                myBot.runAction(new SequentialAction(bucket0, pickup1, bucket1, pickup2, bucket2,
+                        pickup3, bucket3, submer4, bucket4, submer5, bucket5
                         ));
 
                 meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/DubSub.java
@@ -0,0 +1,293 @@
+package org.firstinspires.ftc.teamcode.opmodes.autonomous;
+
+import static org.firstinspires.ftc.teamcode.robot.Hobbes.helpers.HobbesConstants.EXTENDO_ARM_ABOVE_PICKUP;
+import static org.firstinspires.ftc.teamcode.robot.Hobbes.helpers.HobbesConstants.EXTENDO_OUT_FULL;
+import static org.firstinspires.ftc.teamcode.robot.Hobbes.helpers.HobbesConstants.EXTENDO_WRIST_PICKUP;
+import static org.firstinspires.ftc.teamcode.robot.Hobbes.helpers.Macros.*;
+
+import static java.lang.Math.PI;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.PinpointDrive;
+import org.firstinspires.ftc.teamcode.robot.Hobbes.Hobbes;
+import org.firstinspires.ftc.teamcode.robot.Hobbes.helpers.HobbesState;
+
+@Autonomous
+
+public class DubSub extends LinearOpMode {
+    // 5 specimen
+    Hobbes hob = null;
+    PinpointDrive drive;
+    boolean gamepadding = true;
+    int state = 0;
+    double x = 0;
+    double y = 0;
+    double swiv = 0;
+
+    double x2 = 0;
+    double y2 = 0;
+    double swiv2 = 0;
+    Gamepad lastGamepad1 = new Gamepad();
+    @Override
+    // runs on init press
+    public void runOpMode() {
+        Telemetry tele = FtcDashboard.getInstance().getTelemetry();
+        // define and init robot
+        hob = new Hobbes();
+        hob.init(hardwareMap);
+        drive = hob.drive;
+        MecanumDrive.PARAMS.headingGain = 16;
+
+        lastGamepad1.copy(gamepad1);
+        while (opModeInInit() && gamepadding) {
+
+            if (state == 0) { telemetry.addLine("First Sample:\n"); telemetry.addData("Y Coord", y); telemetry.addData("X Coord", x); telemetry.addData("Angle", swiv); telemetry.update(); }
+            if (state == 1) { telemetry.addLine("Second Sample:\n"); telemetry.addData("Y Coord", y2); telemetry.addData("X Coord", x2); telemetry.addData("Angle", swiv2); telemetry.update(); }
+            if (state == 2) {telemetry.addLine("All set!"); telemetry.update(); gamepadding = false;}
+
+            if (gamepad1.dpad_up && !lastGamepad1.dpad_up) {
+                if (state == 0) y++;
+                if (state == 1) y2++;
+            }
+            if (gamepad1.dpad_down && !lastGamepad1.dpad_down) {
+                if (state == 0) y--;
+                if (state == 1) y2--;
+            }
+            if (gamepad1.dpad_right && !lastGamepad1.dpad_right) {
+                if (state == 0) x++;
+                if (state == 1) x2++;
+            }
+            if (gamepad1.dpad_left && !lastGamepad1.dpad_left) {
+                if (state == 0) x--;
+                if (state == 1) x2--;
+            }
+            if (gamepad1.a && !lastGamepad1.a) {
+                if (state == 0) swiv+=15;
+                if (state == 1) swiv2+=15;
+            }
+            if (gamepad1.b && !lastGamepad1.b) {
+                if (state == 0) swiv-=15;
+                if (state == 1) swiv2-=15;
+            }
+
+            telemetry.addLine("Left bumper to accept");
+            telemetry.addLine("Right bumper to go back");
+            telemetry.update();
+
+            lastGamepad1.copy(gamepad1);
+             if (gamepad1.left_bumper) state++;
+            if (gamepad1.right_bumper) state--;
+        }
+        swiv = 0.05/15 * swiv;
+        swiv2 = 0.05/15 * swiv2;
+        TrajectoryActionBuilder b0 = drive.actionBuilder(new Pose2d(0, 0, 0))
+                .setTangent(PI)
+                .lineToX(-10);
+        //Splines for first spike mark and first bucket
+        TrajectoryActionBuilder s1 = b0.endTrajectory().fresh()
+                .setTangent(PI/2)
+                .splineToConstantHeading(new Vector2d(-11, 10), PI/2)
+                .splineToLinearHeading(new Pose2d(-11, 18, PI/2), PI/2, null, new ProfileAccelConstraint(-20, 20));
+
+        TrajectoryActionBuilder b1 = s1.endTrajectory().fresh()
+                .setTangent(-PI/2)
+                .splineToLinearHeading(new Pose2d(-15, 8, PI/4), -3*PI/4);
+        //second spike mark
+        TrajectoryActionBuilder s2 = b1.endTrajectory().fresh()
+                .setTangent(PI/2)
+                .splineToLinearHeading(new Pose2d(-20, 18, PI/2), PI/2, null, new ProfileAccelConstraint(-20, 20))
+                ;
+
+        TrajectoryActionBuilder b2 = s2.endTrajectory().fresh()
+                .setTangent(-PI/2)
+                .splineToLinearHeading(new Pose2d(-28, 12, PI/2), -PI/2);
+
+        TrajectoryActionBuilder s3 = b2.endTrajectory().fresh()
+                .setTangent(0)
+                .splineToLinearHeading(new Pose2d(-12, 27, Math.toRadians(145)), Math.toRadians(145), null, new ProfileAccelConstraint(-20, 20));
+
+        TrajectoryActionBuilder b3 = s3.endTrajectory().fresh()
+                .setTangent(-PI/2)
+                .splineToLinearHeading(new Pose2d(-15, 8, PI/4), -3*PI/4);
+
+
+        TrajectoryActionBuilder s4 = b3.endTrajectory().fresh()
+                .setTangent(PI/4)
+                .splineToLinearHeading(new Pose2d(6+x, 43+y, 0), 0, null, new ProfileAccelConstraint(-50, 50))
+                ;
+        TrajectoryActionBuilder b4 = s4.endTrajectory().fresh()
+                .setTangent(PI)
+                .splineToLinearHeading(new Pose2d(-13, 15, PI/4), -3*PI/4, null, new ProfileAccelConstraint(-40, 80));
+
+        TrajectoryActionBuilder s5 = b4.endTrajectory().fresh()
+                .setTangent(PI/4)
+                .splineToLinearHeading(new Pose2d(6+x2, 43+y2, 0), 0, null, new ProfileAccelConstraint(-50, 50));
+
+        TrajectoryActionBuilder b5 = s5.endTrajectory().fresh()
+                .setTangent(PI)
+                .splineToLinearHeading(new Pose2d(-13, 15, PI/4), -3*PI/4, null, new ProfileAccelConstraint(-40, 80));
+
+
+
+        //build all of the splines
+        Action bucket0 = b0.build();
+        Action pickup1 = s1.build();
+        Action bucket1 = b1.build();
+        Action pickup2 = s2.build();
+        Action bucket2 = b2.build();
+        Action pickup3 = s3.build();
+        Action bucket3 = b3.build();
+        Action submer4 = s4.build();
+        Action bucket4 = b4.build();
+        Action submer5 = s5.build();
+        Action bucket5 = b5.build();
+
+
+
+
+        //build all of the splines
+
+        hob.runMacro(FULL_TRANSFER4);
+        while (opModeInInit()) {
+            hob.tick();
+        }
+        waitForStart();
+
+        Actions.runBlocking(
+                new ParallelAction(
+                        new SequentialAction(
+                                hob.actionMacro(FULL_TRANSFER4),
+                                new ParallelAction(
+                                        bucket0,
+                                        hob.actionMacroTimeout(SLIDES_DEPOSIT, 2),
+                                        hob.actionMacroTimeout(OPEN_CLAW, 1300)
+                                ),
+
+                                new ParallelAction(
+                                        pickup1,
+                                        hob.actionMacroTimeout(SLIDES_DOWN_AND_EXTENDO_SAMPLE,300)
+                                ),
+
+                                hob.actionMacro(EXTENDO_CLAW_BEFORE_PICKUP_AUTO),
+                                hob.actionWait(200), // tune
+                                new ParallelAction(
+                                        bucket1,
+                                        new SequentialAction(
+                                                hob.actionMacro(FULL_TRANSFER),
+                                                hob.actionWait(1500), // tune
+                                                hob.actionMacro(SLIDES_DEPOSIT_AUTO_SAMPS)
+                                        )
+                                ),
+                                hob.actionWait(1200), // tune
+                                hob.actionMacro(OPEN_CLAW),
+
+                                new ParallelAction(
+                                        pickup2,
+                                        hob.actionMacroTimeout(SLIDES_DOWN,300),
+                                        hob.actionMacroTimeout(EXTENDO_CLAW_OVER_SAMPLE_AUTO,800)
+                                ),
+
+                                hob.actionMacro(EXTENDO_CLAW_BEFORE_PICKUP_AUTO),
+                                hob.actionWait(200), // tune
+
+                                new ParallelAction(
+                                        bucket2,
+                                        new SequentialAction(
+                                                hob.actionMacro(FULL_TRANSFER),
+                                                hob.actionWait(1500),
+                                                hob.actionMacro(SLIDES_DEPOSIT_AUTO_SAMPS)
+                                        )
+                                ),
+                                hob.actionWait(1200), // tune
+                                hob.actionMacro(OPEN_CLAW),
+
+                                new ParallelAction(
+                                        pickup3,
+                                        hob.actionMacroTimeout(SLIDES_DOWN,900),
+                                        hob.actionMacroTimeout(EXTENDO_CLAW_OVER_SAMPLE_AUTO_THIRD,1400)
+                                ),
+
+                                hob.actionMacro(EXTENDO_CLAW_BEFORE_PICKUP_AUTO), // CHANGE MACRO FOR SWIVEL
+                                hob.actionWait(200), // tune
+
+                                new ParallelAction(
+                                        bucket3,
+                                        new SequentialAction(
+                                                hob.actionMacro(FULL_TRANSFER),
+                                                hob.actionWait(1500),  // tune
+                                                hob.actionMacro(SLIDES_DEPOSIT_AUTO_SAMPS)
+                                        )
+                                ),
+                                hob.actionWait(1200), // tune
+                                hob.actionMacro(OPEN_CLAW),
+
+                                new ParallelAction(
+                                        submer4,
+                                        hob.actionMacroTimeout(SLIDES_DOWN,300)
+                                ),
+                                //hob.actionWait(1000), // tune
+                                hob.actionMacro(new HobbesState(EXTENDO_OUT_FULL, EXTENDO_ARM_ABOVE_PICKUP, EXTENDO_WRIST_PICKUP, null, null, null, null, null, null, 0.3+swiv, null)),
+                                hob.actionWait(500),
+                                hob.actionMacro(EXTENDO_CLAW_BEFORE_PICKUP), // CHANGE MACRO FOR SWIVEL
+                                hob.actionWait(200), // tune
+
+                                new ParallelAction(
+                                        bucket4,
+                                        new SequentialAction(
+                                                hob.actionMacro(FULL_TRANSFER),
+                                                hob.actionWait(1300),  // tune
+                                                hob.actionMacro(SLIDES_DEPOSIT)
+                                        )
+                                ),
+
+                                hob.actionWait(400), // tune
+                                hob.actionMacro(OPEN_CLAW),
+                                hob.actionWait(300),
+
+                                //
+                                new ParallelAction(
+                                        submer5,
+                                        hob.actionMacroTimeout(SLIDES_DOWN,300)
+                                ),
+                                //hob.actionWait(1000), // tune
+                                hob.actionMacro(new HobbesState(EXTENDO_OUT_FULL,
+                                        EXTENDO_ARM_ABOVE_PICKUP, EXTENDO_WRIST_PICKUP,
+                                        null, null, null,
+                                        null, null, null, 0.3+swiv2, null)),
+                                hob.actionWait(500),
+                                hob.actionMacro(EXTENDO_CLAW_BEFORE_PICKUP), // CHANGE MACRO FOR SWIVEL
+                                hob.actionWait(200), // tune
+
+                                new ParallelAction(
+                                        bucket5,
+                                        new SequentialAction(
+                                                hob.actionMacro(FULL_TRANSFER),
+                                                hob.actionWait(1300),  // tune
+                                                hob.actionMacro(SLIDES_DEPOSIT)
+                                        )
+                                ),
+
+                                hob.actionWait(400), // tune
+                                hob.actionMacro(OPEN_CLAW),
+                                hob.actionWait(300),
+
+
+                                hob.finishAction()),
+                        hob.actionTick()));
+    }
+
+}
