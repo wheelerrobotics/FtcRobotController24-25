4bf73e2b277990f6498d2f99f8f468ef7e272edb
davidyyyyyyyyy <dbattlecardemil@gmail.com>
2025-08-26 13:34:34 -0400

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Hobbes/Odo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Hobbes/Odo.java
@@ -272,56 +272,7 @@ public class Odo extends Meccanum implements Robot {
     - Would be sick if we actually use tensorflow models. Could be useful for object detection of the junctions.
 
      */
-    private class AprilThread {// extends Thread {
 
-        public ArrayList<AprilTagDetection> detections = new ArrayList<>();
-        public BotVision bv = new BotVision();
-        public AprilTagDetectionPipeline atdp =  new AprilTagDetectionPipeline(0.166, 578.272, 578.272, 402.145, 221.506);
-
-        int numFramesWithoutDetection = 0;
-        final int DECIMATION_LOW = 2;
-        final int DECIMATION_HIGH = 3;
-        final float THRESHOLD_HIGH_DECIMATION_RANGE_METERS = 2.0f;
-        final int THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION = 7;
-
-        public void start() {
-            bv.init(hw, atdp);
-
-
-        }
-        public int getDetected(){
-            return checkDetections();
-        }
-        public int checkDetections() {
-            detections = atdp.getDetectionsUpdate();
-            if (detections != null) {
-
-                // If we don't see any tags
-                if (detections.size() == 0) {
-                    numFramesWithoutDetection++;
-
-                    // If we haven't seen a tag for a few frames, lower the decimation
-                    // so we can hopefully pick one up if we're e.g. far back
-                    if (numFramesWithoutDetection >= THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION) {
-                        atdp.setDecimation(DECIMATION_LOW);
-                    }
-                }
-                // We do see tags!
-                else {
-                    numFramesWithoutDetection = 0;
-
-                    // If the target is within 1 meter, turn on high decimation to
-                    // increase the frame rate
-                    if (detections.get(0).pose.z < THRESHOLD_HIGH_DECIMATION_RANGE_METERS) {
-                        atdp.setDecimation(DECIMATION_HIGH);
-                    }
-                }
-
-            }
-
-            return ((detections != null && detections.size() > 0) ? detections.get(0).id : 0);
-        }
-    }
 
     public void tickPID() {
         pt.tick();
