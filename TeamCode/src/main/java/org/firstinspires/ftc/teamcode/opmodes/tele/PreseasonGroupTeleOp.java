a4a952023c559fdfe0e3fdfa5f73f1495ad55fea
darsh7378274 <adarsh.presanna@gmail.com>
2025-09-09 16:56:21 -0400

    iun

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/PreseasonGroupTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/PreseasonGroupTeleOp.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmodes.tele;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
+import com.qualcomm.robotcore.hardware.Servo;
+@Config
 @TeleOp
 public class PreseasonGroupTeleOp extends OpMode {
 
@@ -12,6 +14,9 @@ public class PreseasonGroupTeleOp extends OpMode {
     DcMotor frontRight;
     DcMotor backLeft;
     DcMotor backRight;
+    DcMotor arm;
+
+    Servo claw;
 
     @Override
     public void init() {
@@ -19,6 +24,9 @@ public class PreseasonGroupTeleOp extends OpMode {
         frontRight = hardwareMap.dcMotor.get("frontRight");
         backLeft = hardwareMap.dcMotor.get("backLeft");
         backRight = hardwareMap.dcMotor.get("backRight");
+        arm = hardwareMap.dcMotor.get("arm");
+
+        claw = hardwareMap.servo.get("claw");
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -27,9 +35,16 @@ public class PreseasonGroupTeleOp extends OpMode {
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
 
 
     }
+
+    public static double clawPosOpen = 0.05;
+    public static double clawPosClosed = 0;
 
     @Override
     public void start() {
@@ -38,6 +53,7 @@ public class PreseasonGroupTeleOp extends OpMode {
 
     @Override
     public void loop() {
+
         double y = -gamepad1.left_stick_y;
         double x = gamepad1.left_stick_x;
         double rx = gamepad1.right_stick_x;
@@ -46,6 +62,17 @@ public class PreseasonGroupTeleOp extends OpMode {
         backLeft.setPower(y - x + rx);
         frontRight.setPower(y - x - rx);
         backRight.setPower(y + x - rx);
+
+        if (gamepad2.a) {
+            claw.setPosition(clawPosOpen);
+        }
+        if (gamepad2.b) {
+            claw.setPosition(clawPosClosed);
+        }
+
+        double armValue = gamepad2.left_stick_y;
+        arm.setPower(armValue*0.5);
+
 
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/RobotIn30Hrs.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.opmodes.tele;
+
+
+import static java.lang.Math.abs;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class RobotIn30Hrs extends LinearOpMode {
+
+    Servo intakeRight;
+    Servo intakeLeft;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotor rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        DcMotor leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        DcMotor Launcher = hardwareMap.get(DcMotor.class, "launcher");
+
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+}
+
+
