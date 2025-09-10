512e72917a9e0ccb05f60d6131eaec2ba8b5f640
darsh7378274 <adarsh.presanna@gmail.com>
2025-09-10 16:12:00 -0400

    uytgiobu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/RobotIn30Hrs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/RobotIn30Hrs.java
@@ -1,44 +1,102 @@
 package org.firstinspires.ftc.teamcode.opmodes.tele;
 
 
-import static java.lang.Math.abs;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import static java.lang.Math.PI;
+import static java.lang.Math.abs;
+import static java.lang.Math.atan2;
+import static java.lang.Math.pow;
+
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
+
+import java.util.Deque;
+import java.util.LinkedList;
 @TeleOp
-public class RobotIn30Hrs extends LinearOpMode {
+public class RobotIn30Hrs extends OpMode {
+    Gamepad lastGamepad1 = new Gamepad(), lastGamepad2 = new Gamepad();
+    Deque<Gamepad> gamepad1History = new LinkedList<>(), gamepad2History = new LinkedList<>();
 
-
-
+    DcMotor motorFrontLeft;
+    DcMotor motorBackLeft;
+    DcMotor motorFrontRight;
+    DcMotor motorBackRight;
+    DcMotor intake;
+    DcMotor launcher;
 
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    // runs on init press
+    public void init() {
+        // define and init robot
+        motorFrontLeft = (DcMotorEx) hardwareMap.dcMotor.get("leftFront");
+        motorBackLeft = (DcMotorEx) hardwareMap.dcMotor.get("leftBack");
+        motorFrontRight = (DcMotorEx) hardwareMap.dcMotor.get("rightFront");
+        motorBackRight = (DcMotorEx) hardwareMap.dcMotor.get("rightBack");
+        intake = (DcMotorEx) hardwareMap.dcMotor.get("intake");
+        launcher = (DcMotorEx) hardwareMap.dcMotor.get("launcher");
 
-        DcMotor rightBack = hardwareMap.get(DcMotor.class, "rightBack");
-        DcMotor leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-        DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-        DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-        DcMotor Launcher = hardwareMap.get(DcMotor.class, "launcher");
-        DcMotor Intake = hardwareMap.get(DcMotor.class, "intake");
 
-        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Launcher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        launcher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+    @Override
+    // runs on start press
+    public void start() {
+        // run everything to start positions
+
+    }
+
+    @Override
+    // loops after start press
+    public void loop() {
+        if (gamepad2.start || gamepad1.start) return;
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = -gamepad1.right_stick_x;
+
+        if (!gamepad1.right_bumper) {
+            motorFrontLeft.setPower(y + x + rx);
+            motorBackLeft.setPower(y - x + rx);
+            motorFrontRight.setPower(y - x - rx);
+            motorBackRight.setPower(y + x - rx);
+        } else {
+            motorFrontLeft.setPower(0.3 * (y + x + rx));
+            motorBackLeft.setPower(0.3 * (y - x + rx));
+            motorFrontRight.setPower(0.3 * (y - x - rx));
+            motorBackRight.setPower(0.3 * (y + x - rx));
+        }
+
+        if (gamepad2.right_bumper) {
+            intake.setPower(1);
+        }
+
+        if (gamepad2.left_bumper) {
+            intake.setPower(-1);
+        }
+
+        if (gamepad2.right_bumper) {
+            launcher.setPower(1);
+        }
+
 
     }
 }
-
-
-
