1b8a0cb928794a05b7f4cf5e89174e1e454147c8
darsh7378274 <adarsh.presanna@gmail.com>
2025-10-25 15:14:42 -0400

    Update RPMShooter.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/RPMShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/RPMShooter.java
@@ -28,13 +28,11 @@ public class RPMShooter extends OpMode {
         launcher1 = (DcMotorEx) hardwareMap.dcMotor.get("launcher");
         launcher2 = (DcMotorEx) hardwareMap.dcMotor.get("launcher");
 
-        launcher1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        launcher1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        launcher1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launcher1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        launcher1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        launcher2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        launcher2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        launcher2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launcher2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        launcher2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     @Override
